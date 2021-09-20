8515029b3eac5864f8fc2dc92ce58c3fe36c663c
Karolis Davli <kdavlijevas@gmail.com>
2021-09-20 19:00:46 +0200

    class template init

--- script.js
+++ script.js
@@ -2,36 +2,36 @@ let addABook = document.querySelector('.add');
 let popUp = document.querySelector('.pop-up');
 let cancel = document.querySelector('.cancel');
 let submit = document.querySelector('.submit');
+const myLibrary = [];
 
-let myLibrary = [];
 
-function Book(title, author, pages) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-}
+// Book class template
 
-function addBookToLibrary() {
-  let newBook = createBook();
-  console.log();
-  myLibrary.push(newBook);
-  putBook(newBook);
-  popUp.classList.remove('pop-up-active');
-}
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
 
-// Generate new book object
-function createBook() {
-  let title = document.querySelector('.title').value;
-  let author = document.querySelector('.author').value;
-  let pages = document.querySelector('.pages').value;
-  let read = document.querySelector('.read').value;
+  createBook() {
+    let title = document.querySelector('.title').value;
+    let author = document.querySelector('.author').value;
+    let pages = document.querySelector('.pages').value;
+    let read = document.querySelector('.read').value;
 
-  const newBook = new Book(title, author, pages, read);
-  return newBook;
-}
+    const newBook = new Book(title, author, pages, read);
+    return newBook;
+  }
 
-// Put new book in html
-function putBook(book) {
+  addBookToLibrary() {
+    let newBook = Book.createBook();
+    myLibrary.push(newBook);
+    putBook(newBook);
+    popUp.classList.remove('pop-up-active');
+  }
+
+  putBook() {
     let newBook = createBook();
     let lib = document.querySelector('.library');
 
@@ -61,43 +61,41 @@ function putBook(book) {
     newBookUl.append(newTitleLi, newAuthorLi, newPagesLi);
   
     lib.appendChild(newBookUl);
-}
-
-
-// Create buttons in a div
-function createButtons(book, lib) {
-
-  let buttonsDiv = document.createElement('div');
-  buttonsDiv.classList.add('buttons-div');
-
-  let readButton = document.createElement('button');
-  readButton.classList.add('read-button');
-  readButton.innerText = 'completed';
-
-  // Check if book is read
-  let readResult = checked();
-  if (readResult == true) {
-    readButton.classList.add('read-button-completed')
   }
 
-  readButton.addEventListener('click', () => {
-    readButton.classList.toggle('read-button-completed')
-  })
-
-  let deleteButton = document.createElement('button');
-  deleteButton.classList.add('delete-button');
-  deleteButton.innerText = 'delete';
-  deleteButton.addEventListener('click', () => {
-    lib.removeChild(book);
-    myLibrary.splice(myLibrary.indexOf(book), 1);
-    console.table(myLibrary);
-  })
-
-  buttonsDiv.append(readButton, deleteButton);
-  book.appendChild(buttonsDiv);
+  createBook(book, lib) {
+    let buttonsDiv = document.createElement('div');
+    buttonsDiv.classList.add('buttons-div');
+  
+    let readButton = document.createElement('button');
+    readButton.classList.add('read-button');
+    readButton.innerText = 'completed';
+  
+    // Check if book is read
+    let readResult = checked();
+    if (readResult == true) {
+      readButton.classList.add('read-button-completed')
+    }
+  
+    readButton.addEventListener('click', () => {
+      readButton.classList.toggle('read-button-completed')
+    })
+  
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.innerText = 'delete';
+    deleteButton.addEventListener('click', () => {
+      lib.removeChild(book);
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      console.table(myLibrary);
+    })
+  
+    buttonsDiv.append(readButton, deleteButton);
+    book.appendChild(buttonsDiv);
+  }
 }
 
-
+const book1 = new Book();
 
 // Add
 addABook.addEventListener('click', function() {
@@ -110,7 +108,7 @@ cancel.addEventListener('click', function() {
 })
 
 // Submit
-submit.addEventListener('click', addBookToLibrary)
+submit.addEventListener('click', book1.addBookToLibrary())
 
 
 // Read switch
@@ -120,3 +118,5 @@ checkbox.addEventListener('change', checked);
 function checked() {
  return checkbox.checked;
 }
+
+console.log(book1);
